276aef2ebd04cdeebb956eea902013d7a2843a76
nlapinski <nate.lapinski@gmail.com>
2023-02-17 22:08:15 -0800

    build script changes for windows

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
-
-meson build
-ninja -C build
-
-# rm build/game.so
-
-# valac sandbox/*.vala --directory=build --vapidir=build --pkg=virgil --pkg=gmodule-2.0 --ccode --header=build/sandbox.h
-# cc -shared -Ibuild/ -I/usr/include/glib-2.0 -I/usr/lib64/glib-2.0/include -w -O3 -fPIC -pthread build/sandbox/*.c -lm -lgmodule-2.0 -lglib-2.0 -o build/game.so
-
-# rm build/sandbox.h
-
-build/virgil --test-build
+rm build/*.h
+rm build/*.a
+rm build/*.exe
+meson setup build
+#meson setup build --reconfigure
+meson compile -C build
+meson install -C build
+rm *.a
+rm *.exe.a
+./virgil.exe --test-build
